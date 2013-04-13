044172aadccb7dcb2ace8ce480200d86ebccb918
mcastanox <jorikst@gmail.com>
2013-04-13 10:04:13 +0000

    Inicializacion del main del proyecto

---
+++ aima-core/src/main/java/aima/core/environment/otello/Otello.java
@@ -0,0 +1,13 @@
+package aima.core.environment.otello;
+
+public class Otello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
