1d73191b95c13485fa1506e3fdb5f67a64f45e60
jomorenoj <jomorenoj@a56a3de2-1000-9a4d-9a66-a95fae22dca7>
2013-05-09 16:04:34 +0000

    Agregado librerias

--- aima-core/src/main/java/aima/core/environment/otello/OtelloState.java
+++ aima-core/src/main/java/aima/core/environment/otello/OtelloState.java
@@ -1,5 +1,11 @@
 package aima.core.environment.otello;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import aima.core.util.datastructure.XYLocation;
+
 public class OtelloState {
 
 }
