bfd729a4266515bee9fee7fd5255766633121653
jomorenoj <jorikst@gmail.com>
2013-05-17 16:59:37 +0000

    Add graphic interfaz Fix convert disk and valid move Change from pair to XYLocation

--- aima-core/src/main/java/aima/core/environment/otello/OtelloState.java
+++ aima-core/src/main/java/aima/core/environment/otello/OtelloState.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import aima.core.environment.tictactoe.TicTacToeState;
 import aima.core.util.datastructure.Pair;
 import aima.core.util.datastructure.Triplet;
-//import aima.core.util.datastructure.XYLocation;
 import aima.core.util.datastructure.XYLocation;
+//import aima.core.util.datastructure.XYLocation;
 
 
 
@@ -15,35 +16,36 @@ import aima.core.util.datastructure.XYLocation;
  
 public class OtelloState implements Cloneable{
 	
-	private static final short white = 1;
-	private static final short black = 2;
-	private static final short EMPTY = 0;
+	public static final String white = "1";
+	public static final String black = "2";
+	public static final String EMPTY = "0";
 
 	private short leftmoves = 60;
 
 
-	private short board [][] = new short[8][8]; // Tablero/ Board
+	private String board [][] = new String[8][8]; // Tablero/ Board
 	//board = 0 Vacio
 	//board = 1 Blancas
 	//board = 2 Negras
 
-	private short playerToMove = black;
+	private String playerToMove = black;
 
-	private short utility = -1; // 1: win for White, 2: win for Black, 0: draw
+	public String utility = Playing; // 1: win for White, 2: win for Black, 0: draw
+	public static final String Playing = "-1";
 
 
 	public OtelloState(){ // Constructor de la clase, inicaliza el tablero./Class Constructor. Initiate Board.
 	
 		for(int i = 0;i < 8;i++){
 			for(int j = 0;j < 8;j++){
-				board[i][j] = 0;
+				board[i][j] = EMPTY;
 			}
 		}
 		board[3][3] = board[4][4] = white;
 		board[3][4] = board[4][3] = black;
 	}
 	
-	public short getPlayerToMove() {
+	public String getPlayerToMove() {
 		return playerToMove;
 	}
 	
@@ -51,11 +53,11 @@ public class OtelloState implements Cloneable{
 		return board[col][row] == EMPTY;
 	}
 	
-	public short getValue(int col, int row) {
+	public String getValue(int col, int row) {
 		return board[col][row];
 	}
 	
-	public short getUtility() {
+	public String getUtility() {
 		return utility;
 	}
 	
@@ -64,96 +66,226 @@ public class OtelloState implements Cloneable{
 	}
 	
 	public void mark(int col, int row) {
-		if (utility == -1 && getValue(col, row) == EMPTY) {
+		if (utility == Playing && getValue(col, row) == EMPTY) {
+			String temp[][] = new String[8][8];
+			temp = cloneM(board);
 			board[col][row] = playerToMove;
-			convertDisks(col,row);
-			analyzeUtility();
-			playerToMove = (playerToMove == white ? black : white); // modificar
+			if(!convertDisks(col,row)){
+				board = cloneM(temp);	
+			}
+			else{
+				analyzeUtility();
+				playerToMove = (playerToMove == white ? black : white);
+			}
 		}
 	}
 	
-	private void convertDisk(int col, int row){
-		convertLeftTop(col,row);
-		convertLeft(col,row);
-		convertLeftBottom(col,row);
-		convertTop(col,row);
-		convertBottom(col,row);
-		convertRightTop(col,row);
-		convertRight(col,row);
-		convertRightBottom(col,row);
-	}
-
-
-	private void convertLeftTop(int col, int row) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void convertLeft(int col, int row) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void convertLeftBottom(int col, int row) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void convertTop(int col, int row) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void convertBottom(int col, int row) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void convertRightTop(int col, int row) {
-		// TODO Auto-generated method stub
-		
+	
+	
+	private boolean convertDisks(int col, int row){
+		boolean a1,a2,a3,a4,a5,a6,a7,a8;
+		a1 = convertLeftTop(col, row);
+		a2 = convertLeft(col, row);
+		a3 = convertLeftBottom(col, row);
+		a4 = convertTop(col, row);
+		a5 = convertBottom(col, row);
+		a6 = convertRightBottom(col, row);
+		a7 = convertRight(col, row);
+		a8 = convertRightTop(col, row);
+/*		if(	convertLeftTop(col,row) || convertLeft(col,row) || 
+			convertLeftBottom(col,row) || convertTop(col,row) || 
+			convertBottom(col,row) || convertRightTop(col,row) || 
+			convertRight(col,row) || convertRightBottom(col,row))
+			return true;
+		return false;*/
+		if(a1 || a2 || a3 || a4 || a5 || a6 || a7 || a8){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 	
-	private void convertRight(int col, int row) {
+	private boolean convertLeftTop(int col, int row) {
 		// TODO Auto-generated method stub
-		
-	}
-
-	private void convertRightBottom(int col, int row) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private Pair<Boolean, ArrayList<Pair<Short, Short>>> validLeftTopMove(short temp[][], Triplet<Short,Short,Short> delta){
-			
-			ArrayList<Pair<Short, Short>> middledisks = new ArrayList<Pair<Short,Short>>();
-			if(delta.getFirst()>1 && delta.getSecond()>1 && delta.getFirst()<=7 && delta.getSecond()<=7)
-				if( temp[delta.getFirst()-1][delta.getSecond()-1] != delta.getThird() && temp[delta.getFirst()-1][delta.getSecond()-1] != 0 ){
-					int j = delta.getSecond()-1;
-					for(int i = delta.getFirst()-1;i>=0 && j>=0;i--,j--){
-						if(delta.getThird() == temp[i][j])
-							return new Pair<Boolean, ArrayList<Pair<Short, Short>>>((boolean) true, middledisks);
-						else
-							if(temp[i][j] != 0)
-								middledisks.add(new Pair<Short, Short>((short) i, (short) j));
-							else
-								return new Pair<Boolean, ArrayList<Pair<Short, Short>>>((boolean) false, null);
+		String temp[][] = cloneM(board);
+		System.out.println("");
+		if(col>1 && row >1 && col <=7 && row<=7)
+			if(temp[col-1][row-1] != getPlayerToMove() && temp[col-1][row-1] != EMPTY){
+				int j = row-1;
+				for(int i = col-1;i>=0 && row>=0;i--,j--)
+					if(getPlayerToMove() == temp[i][j]){
+						board = cloneM(temp);
+						return true;
 					}
-				}
-			return new Pair<Boolean, ArrayList<Pair<Short, Short>>>((boolean) false, null);
-			//A9
+					else{
+						if(temp[i][j] != EMPTY)
+							temp[i][j] = getPlayerToMove();
+						else
+							return false;
+					}
+				
+			}
+		return false;		
 	}
+
+	private boolean convertLeft(int col, int row) {
+		// TODO Auto-generated method stub
+		String temp [][] =  cloneM(board);
+		if(col>1 && row >=1 && col <=7 && row<=7)
+			if(temp[col-1][row] != getPlayerToMove() && temp[col-1][row] != EMPTY){
+				for(int i = col-1;i>=0;i--)
+					if(getPlayerToMove() == temp[i][row]){
+						board = cloneM(temp);
+						return true;
+					}
+					else{
+						if(temp[i][row] != EMPTY)
+							temp[i][row] = getPlayerToMove();
+						else
+							return false;
+					}
+				
+			}
+		return false;
+	}
+
+	private boolean convertLeftBottom(int col, int row) {
+		// TODO Auto-generated method stub
+		String temp [][] =  cloneM(board);
+		if(col>1 && row >=1 && col <=7 && row<7)
+			if(temp[col-1][row+1] != getPlayerToMove() && temp[col-1][row+1] != EMPTY){
+				int j = row+1;
+				for(int i = col-1;i>=0 && row>=0;i--,j++)
+					if(getPlayerToMove() == temp[i][j]){
+						board = cloneM(temp);
+						return true;
+					}
+					else{
+						if(temp[i][j] != EMPTY)
+							temp[i][j] = getPlayerToMove();
+						else
+							return false;
+					}
+				
+			}
+		return false;
+	}
+
+	private boolean convertTop(int col, int row) {
+		// TODO Auto-generated method stub
+		String temp [][] =  cloneM(board);
+		if(col>=1 && row >1 && col <=7 && row<=7)
+			if(temp[col][row-1] != getPlayerToMove() && temp[col][row-1] != EMPTY){
+				for(int j = row-1;j>=0;j--)
+					if(getPlayerToMove() == temp[col][j]){
+						board = cloneM(temp);
+						return true;
+					}
+					else{
+						if(temp[col][j] != EMPTY)
+							temp[col][j] = getPlayerToMove();
+						else
+							return false;
+					}
+			}
+		return false;
+	}
+
+	private boolean convertBottom(int col, int row) {
+		// TODO Auto-generated method stub
+		String temp [][] =  cloneM(board);
+		if(col>=1 && row >=1 && col <=7 && row<7)
+			if(temp[col][row+1] != getPlayerToMove() && temp[col][row+1] != EMPTY){
+				for(int j = row+1;j<=8;j++)
+					if(getPlayerToMove() == temp[col][j]){
+						board = cloneM(temp);
+						return true;
+					}
+					else{
+						if(temp[col][j] != EMPTY)
+							temp[col][j] = getPlayerToMove();
+						else
+							return false;
+					}
+				
+			}
+		return false;
+	}
+
+	private boolean convertRightTop(int col, int row) {
+		// TODO Auto-generated method stub
+		String temp [][] =  cloneM(board);
+		if(col>=1 && row >1 && col <7 && row<=7)
+			if(temp[col+1][row-1] != getPlayerToMove() && temp[col+1][row-1] != EMPTY){
+				int j = row-1;
+				for(int i = col+1;i<=8 && j>=0;i++,j--)
+					if(getPlayerToMove() == temp[i][j]){
+						board = cloneM(temp);
+						return true;
+					}
+					else{
+						if(temp[i][j] != EMPTY)
+							temp[i][j] = getPlayerToMove();
+						else
+							return false;
+					}
+				
+			}
+		return false;
+	}
+	
+	private boolean convertRight(int col, int row) {
+		// TODO Auto-generated method stub
+		String temp [][] =  cloneM(board);
+		if(col>=1 && row >=1 && col <7 && row<=7)
+			if(temp[col+1][row] != getPlayerToMove() && temp[col+1][row] != EMPTY){
+				for(int i = col+1;i<=8;i++)
+					if(getPlayerToMove() == temp[i][row]){
+						board = cloneM(temp);
+						return true;
+					}
+					else{
+						if(temp[i][row] != EMPTY)
+							temp[i][row] = getPlayerToMove();
+						else
+							return false;
+					}
+				
+			}
+		return false;
+	}
+
+	private boolean convertRightBottom(int col, int row) {
+		// TODO Auto-generated method stub
+		String temp [][] =  cloneM(board);
+		if(col>=1 && row >=1 && col <7 && row<7)
+			if(temp[col+1][row+1] != getPlayerToMove() && temp[col+1][row+1] != EMPTY){
+				int j = row+1;
+				for(int i = col+1;i<=8 && row<=8;i++,j++)
+					if(getPlayerToMove() == temp[i][j]){
+						board = cloneM(temp);
+						return true;
+					}
+					else{
+						if(temp[i][j] != EMPTY)
+							temp[i][j] = getPlayerToMove();
+						else
+							return false;
+					}
+				
+			}
+		return false;
+	}
+
+	
 	
 	private void analyzeUtility() {
 		if (leftmoves == 0) 
-			utility = (count() > white ? white : black);
-		//else if (getNumberOfMarkedPositions() == 64) {
-		//	utility = 0; 
-	
-		//}
+			utility = (count().equalsIgnoreCase(white) ? white : black);
 	}
 	
-	private short count(){
+	private String count(){
 		short countw = 0;
 		short countb = 0;
 		for(int i = 0;i<8;i++){
@@ -170,8 +302,41 @@ public class OtelloState implements Cloneable{
 			if (countb > countw)
 				return black;
 			else
-				return 0;
+				return EMPTY;
 		
+	}
+
+	public List<XYLocation> getUnMarkedPositions() {
+		// TODO Auto-generated method stub
+		List<XYLocation> result = new ArrayList<XYLocation>();
+		for(int i = 0;i<8;i++){
+			for(int j = 0;j<8;j++){
+				if(getValue(i, j) == EMPTY )
+					result.add(new XYLocation(i, j));
+			}
+		}
+		return result;
+	}
+	
+	@Override
+	public OtelloState clone() {
+		OtelloState copy = null;
+		try {
+			copy = (OtelloState) super.clone();
+			copy.board = (String[][]) board.clone();
+			for(int i = 0;i<board.length;i++)
+				copy.board[i] = board[i].clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace(); // should never happen...
+		}
+		return copy;
+	}
+	
+	private String[][] cloneM(String[][] tab){
+		String copy[][] = new String[8][8];
+		for(int i = 0;i<tab.length;i++)
+			copy[i] = tab[i].clone();
+		return copy;
 	}
 
 }
--- aima-gui/src/main/java/aima/gui/demo/search/OtelloDemo.java
+++ aima-gui/src/main/java/aima/gui/demo/search/OtelloDemo.java
@@ -1,5 +1,47 @@
 package aima.gui.demo.search;
 
+import aima.core.environment.otello.OtelloGame;
+import aima.core.environment.otello.OtelloState;
+import aima.core.search.adversarial.AdversarialSearch;
+import aima.core.search.adversarial.AlphaBetaSearch;
+import aima.core.search.adversarial.MinimaxSearch;
+import aima.core.util.datastructure.XYLocation;
+
 public class OtelloDemo {
+	public static void main(String[] args) {
+		System.out.println("OTELLO DEMO");
+		System.out.println("");
+		startMinimaxDemo();
+		startAlphaBetaDemo();
+	}
+
+	private static void startMinimaxDemo() {
+		System.out.println("MINI MAX DEMO\n");
+		OtelloGame game = new OtelloGame();
+		OtelloState currState = game.getInitialState();
+		AdversarialSearch<OtelloState, XYLocation> search = MinimaxSearch.createFor(game);
+		while (!(game.isTerminal(currState))) {
+			System.out.println(game.getPlayer(currState) + "  playing ... ");
+			XYLocation action = search.makeDecision(currState);
+			currState = game.getResult(currState, action);
+			System.out.println(currState);
+		}
+		System.out.println("MINI MAX DEMO done");
+	}
+
+	private static void startAlphaBetaDemo() {
+		System.out.println("ALPHA BETA DEMO\n");
+		OtelloGame game = new OtelloGame();
+		OtelloState currState = game.getInitialState();
+		AdversarialSearch<OtelloState, XYLocation> search = AlphaBetaSearch
+				.createFor(game);
+		while (!(game.isTerminal(currState))) {
+			System.out.println(game.getPlayer(currState) + "  playing ... ");
+			XYLocation action = search.makeDecision(currState);
+			currState = game.getResult(currState, action);
+			System.out.println(currState);
+		}
+		System.out.println("ALPHA BETA DEMO done");
+	}
 
 }
